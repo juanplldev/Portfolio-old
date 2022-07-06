cb424a9b2cec96eb9adfe2e45e02596102f61826
JuanPablo-Llorente <juanpablollorentej@gmail.com>
2022-07-06 18:26:29 -0300

    nabvar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,26 +4,30 @@ import {Link, useNavigate} from "react-router-dom";
 // Files
 import {scrollToAbout, scrollToStack, scrollToProjects, scrollToContact} from "../../handlers/handlers";
 import CloseTagIcon from "../../img/CloseTagIcon.png";
-// import About from "../../img/About.png";
-// import Stack from "../../img/Stack.png";
-// import Projects from "../../img/Projects.png";
-// import Contact from "../../img/Contact.png";
+import About from "../../img/About.png";
+import Stack from "../../img/Stack.png";
+import Projects from "../../img/Projects.png";
+import Contact from "../../img/Contact.png";
 import styles from "./NavBar.module.css";
 
 
 function NavBar()
 {
     const navigate = useNavigate();
-    const height = window.innerHeight;
+    const [height, setHeight] = useState(window.innerHeight);
+    const [width, setWidth] = useState(window.innerWidth);
+    const [actualHeight, setActualHeight] = useState(window.visualViewport.pageTop);
+    
     const aboutIndicator = height - 70;
-    const stackIndicator = window.innerWidth <= 1024 ? height * 3 - 70 : height * 2 - 70;
-    const projectsIndicator = window.innerWidth <= 1024 ? height * 5 - 70 : height * 3 - 70;
+    const stackIndicator = width <= 1024 ? height * 3 - 70 : height * 2 - 70;
+    const projectsIndicator = width <= 1024 ? height * 5 - 70 : height * 3 - 70;
     const contactIndicator = projectsIndicator + height;
     const homeLocation = window.location.pathname === "/home" ? true : false;
-    const [actualHeight, setActualHeight] = useState(window.visualViewport.pageTop);
     
     window.addEventListener("scroll", () => {
         setActualHeight(window.visualViewport.pageTop);
+        setHeight(window.innerWidth);
+        setWidth(window.innerWidth);
     });
     
     async function handleScrollAbout()
@@ -60,13 +64,37 @@ function NavBar()
                 </div>
                 
                 <div className={styles.Menu}>
-                    <button onClick={handleScrollAbout} className={homeLocation && actualHeight < aboutIndicator ? styles.ActiveButton : styles.Button}>About</button>
+                    <button onClick={handleScrollAbout} className={homeLocation && actualHeight < aboutIndicator ? styles.ActiveButton : styles.Button}>
+                        {
+                            width <= 550 ? <img src={About} alt="About" />
+                            :
+                            "About"
+                        }
+                    </button>
                     
-                    <button onClick={handleScrollStack} className={homeLocation && actualHeight >= aboutIndicator && actualHeight < stackIndicator ? styles.ActiveButton : styles.Button}>Stack</button>
+                    <button onClick={handleScrollStack} className={homeLocation && actualHeight >= aboutIndicator && actualHeight < stackIndicator ? styles.ActiveButton : styles.Button}>
+                        {
+                            width <= 550 ? <img src={Stack} alt="Stack" />
+                            :
+                            "Stack"
+                        }
+                    </button>
                     
-                    <button onClick={handleScrollProjects} className={homeLocation && actualHeight >= stackIndicator && actualHeight < projectsIndicator ? styles.ActiveButton : styles.Button}>Projects</button>
+                    <button onClick={handleScrollProjects} className={homeLocation && actualHeight >= stackIndicator && actualHeight < projectsIndicator ? styles.ActiveButton : styles.Button}>
+                        {
+                            width <= 550 ? <img src={Projects} alt="Projects" />
+                            :
+                            "Projects"
+                        }
+                    </button>
                     
-                    <button onClick={handleScrollContact} className={homeLocation && actualHeight >= projectsIndicator && actualHeight < contactIndicator ? styles.ActiveButton : styles.Button}>Contact</button>
+                    <button onClick={handleScrollContact} className={homeLocation && actualHeight >= projectsIndicator && actualHeight < contactIndicator ? styles.ActiveButton : styles.Button}>
+                        {
+                            width <= 550 ? <img src={Contact} alt="Contact" />
+                            :
+                            "Contact"
+                        }
+                    </button>
                 </div>
                 
                 <div className={styles.SwitcherContainer}>
