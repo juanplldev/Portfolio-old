7b53b6d892bc80b40b33d5e8ae900873641d174d
JuanPablo-Llorente <juanpablollorentej@gmail.com>
2022-07-06 14:20:42 -0300

    navbar indicator

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 // Dependencies
-import React from "react";
+import React, {useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 // Files
 import {scrollToAbout, scrollToStack, scrollToProjects, scrollToContact} from "../../handlers/handlers";
@@ -14,6 +14,17 @@ import styles from "./NavBar.module.css";
 function NavBar()
 {
     const navigate = useNavigate();
+    const height = window.innerHeight;
+    const aboutIndicator = height - 70;
+    const stackIndicator = window.innerWidth <= 1024 ? height * 3 - 70 : height * 2 - 70;
+    const projectsIndicator = window.innerWidth <= 1024 ? height * 5 - 70 : height * 3 - 70;
+    const contactIndicator = projectsIndicator + height;
+    const homeLocation = window.location.pathname === "/home" ? true : false;
+    const [actualHeight, setActualHeight] = useState(window.visualViewport.pageTop);
+    
+    window.addEventListener("scroll", () => {
+        setActualHeight(window.visualViewport.pageTop);
+    });
     
     async function handleScrollAbout()
     {
@@ -49,13 +60,13 @@ function NavBar()
                 </div>
                 
                 <div className={styles.Menu}>
-                    <button onClick={handleScrollAbout} className={styles.AboutMe}>About</button>
+                    <button onClick={handleScrollAbout} className={homeLocation && actualHeight < aboutIndicator ? styles.ActiveButton : styles.Button}>About</button>
                     
-                    <button onClick={handleScrollStack} className={styles.MyStack}>Stack</button>
+                    <button onClick={handleScrollStack} className={homeLocation && actualHeight >= aboutIndicator && actualHeight < stackIndicator ? styles.ActiveButton : styles.Button}>Stack</button>
                     
-                    <button onClick={handleScrollProjects} className={styles.Projects}>Projects</button>
+                    <button onClick={handleScrollProjects} className={homeLocation && actualHeight >= stackIndicator && actualHeight < projectsIndicator ? styles.ActiveButton : styles.Button}>Projects</button>
                     
-                    <button onClick={handleScrollContact} className={styles.Contact}>Contact</button>
+                    <button onClick={handleScrollContact} className={homeLocation && actualHeight >= projectsIndicator && actualHeight < contactIndicator ? styles.ActiveButton : styles.Button}>Contact</button>
                 </div>
                 
                 <div className={styles.SwitcherContainer}>
